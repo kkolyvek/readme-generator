d620609358774dbd60426b19388a78aed9081ab7
Koppi Kolyvek <kk674@cornell.edu>
2021-07-15 16:11:22 -0700

    initial commit to repo

---
+++ index.js
@@ -0,0 +1,89 @@
+// ---------------------
+// INIT GLOBAL VARIABLES
+// ---------------------
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions = [
+    // USER INPUTS INCLUDE:
+    // - project title
+    // - Github username
+    // - email
+    // - credits given
+    // - license choice
+    {
+        type: 'input',
+        message: 'Enter your full name:',
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'Enter project title:',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub username:',
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: 'Enter the GitHub repository name:',
+        name: 'repo'
+    },
+    {
+        type: 'input',
+        message: 'Enter your email:',
+        name: 'email'
+    },
+    {
+        type: 'confirm',
+        message: 'Are you giving credit to any contributors?',
+        name: 'credit'
+    },
+    {
+        type: 'list',
+        message: 'Select license:',
+        choices: [
+                'Unlicensed',
+                'MIT License',
+                'Apache License',
+                'GNU GPL License'
+            ],
+        name: 'license'
+    }
+];
+
+
+
+// ---------
+// FUNCTIONS
+// ---------
+// TODO: Create a function to write README file
+const writeToFile = (fileName, data) => {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.log(err) : console.log('Success! ReadMe file created.');
+    });
+};
+
+// TODO: Create a function to initialize app
+const init = () => {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            console.log('Generating file...');
+
+            console.log(response)
+
+            // generate markdown file
+            const md = generateMarkdown(response);
+
+            // all done, create file
+            writeToFile('readme.md', md);
+        });
+};
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,162 @@
+d = new Date();
+
+// Render title and description
+const renderTitle = (title, github, repo) => {
+  return `
+<h1 align="center"><strong>${title}</strong></h1>
+
+[languages badge](https://img.shields.io/github/languages/top/${github}/${repo}) [license badge](https://img.shields.io/github/license/${github}/${repo})
+
+## Description`;
+};
+
+// Render table of contents based on whether credit is being given
+const renderTOC = creditBool => {
+  if (creditBool) {
+    return `
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Credits](#credits)
+4. [License](#license)
+5. [Contributing](#contributing)
+6. [Tests](#tests)
+7. [Questions](#questions)
+
+## Installation
+
+## Usage`;
+  } else {
+    return `
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions)
+    
+## Installation
+
+## Usage`;
+  };
+};
+
+// Render license according to input
+const renderLicense = (license, name) => {
+  if (license === 'Unlicensed') {
+    return `
+## License
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>`;
+  } else if (license === 'MIT License') {
+    return `
+MIT License
+
+Copyright (c) ${d.getFullYear()} ${name}
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`;
+  } else if (license === 'Apache License') {
+    return `
+Copyright ${d.getFullYear()} ${name}
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  > <http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.`;
+  } else if (license === 'GNU GPL License') {
+    return `
+Copyright (C) ${d.getFullYear()} ${name}
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.`;
+  };
+};
+
+// Render final section using data
+const renderQuestions = (github, email) => {
+  return `
+---
+
+## Contributing
+
+## Tests
+
+## Questions
+
+For further questions and comments, please reach out through [GitHub](${github}) or [Email](${email}).`;
+};
+
+// Assemble
+function generateMarkdown(data) {
+  // Render title
+  const title = renderTitle(data.title, data.github, data.repo);
+  // Render table of contents
+  const toc = renderTOC(data.credit);
+  // Render license
+  const license = renderLicense(data.license);
+  // Render questions
+  const questions = renderQuestions(data.github, data.email);
+
+  return title + toc + license + questions;
+}
+
+module.exports = generateMarkdown;
